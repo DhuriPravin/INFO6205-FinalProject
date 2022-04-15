80e369b5bfe601e8e4a48fc4d53f1900d98e38e0
Pravin Dhuri <dhuri.p@northeastern.edu>
2022-04-15 19:40:21 -0400

    PSA_FinalProject: Init Constructor for TicTacToeGame.java
    Signed-off-by: Pravin Dhuri <dhuri.p@northeastern.edu>

---
+++ src/game/Participant.java
@@ -0,0 +1,57 @@
+package src.game;
+
+public abstract class Participant {
+
+	private int LoseCounts;
+	private int WinCounts;
+	private int DrawCounts;
+	private int GamesCount;
+	private char[] sWindow;
+	private int currSIdx;
+	protected ValuesOfCell currMov;
+
+	public static final char Winn = 'w';
+	public static final char conditionLose = 'l';
+	public static final char DDraw = 'd';
+
+	public Participant(){
+		LoseCounts = 0;
+		WinCounts = 0;
+		DrawCounts = 0;
+		GamesCount = 0;
+		sWindow = new char[200];
+		currSIdx= 0;
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ src/game/TicTacToeGame.java
@@ -0,0 +1,94 @@
+package src.game;
+
+import src.utils.Utils;
+
+public class TicTacToeGame {
+
+	private ValuesOfCell[] board;
+
+	private int totalRounds; // records total rounds
+
+	private StatesOfAnyGame statesOfAnyGame; //records the current state of the game.
+
+	public final int rows;
+
+	public final int columns;
+
+	public final int totalsCellsRequiredToWin; // number of indexCell of the same type * that must be aligned to conditionWin the game
+
+	// list of all possible symmetries
+    private  static final Symmetry[] allPossibleSymmetries = {Symmetry.ID, Symmetry.Rotation, Symmetry.Rotation,Symmetry.Rotation, Symmetry.HorizontalSymmetry, Symmetry.Rotation,Symmetry.Rotation, Symmetry.Rotation};
+
+    private  static final Symmetry[] ALL_TRANSFORMATIONS_NON_SQUARE ={Symmetry.ID, Symmetry.HorizontalSymmetry,Symmetry.VerticalSymmetry, Symmetry.HorizontalSymmetry};
+
+    private  Symmetry[] allSymmetries; // instance to record symmetry
+
+    private int currentTransformation; // number of transformations so far
+
+	public int[] boardTransformation;
+
+	public TicTacToeGame(){ // constructor
+		this(3,3,3);
+	}
+
+	public TicTacToeGame(int rows, int columns){
+		this(rows, columns, 3);
+	}
+
+	public TicTacToeGame(int rows, int columns, int totalsCellsRequiredToWin){
+		this.rows = rows;
+		this.columns = columns;
+		this.totalsCellsRequiredToWin = totalsCellsRequiredToWin;
+		board = new ValuesOfCell[rows*columns];
+		for(int i = 0; i < rows*columns ; i ++) {
+			board[i] = ValuesOfCell.EMPTY;
+		}
+		totalRounds = 0;
+		statesOfAnyGame = StatesOfAnyGame.Playing;
+		if(rows == columns) {
+			allSymmetries = allPossibleSymmetries;
+		} else {
+			allSymmetries = ALL_TRANSFORMATIONS_NON_SQUARE;
+		}
+		boardTransformation = new int[rows*columns];
+		SymmetryReset();
+
+	}
+
+	public TicTacToeGame(TicTacToeGame base, int following){
+
+		rows = base.rows;
+		columns = base.columns;
+		totalsCellsRequiredToWin = base.totalsCellsRequiredToWin;
+
+		if(following < 0 || following >= rows*columns){
+			throw new IllegalArgumentException("Wrong position: " + following);
+		}
+
+		if(base == null){
+			throw new IllegalArgumentException("Wrong base: Null");
+		}
+
+		if(base.board[following] != ValuesOfCell.EMPTY) {
+			throw new IllegalArgumentException("ValueInCell is not empty: " + following + " game " + base);
+		}
+
+		board = new ValuesOfCell[rows*columns];
+		for(int i = 0; i < rows*columns ; i ++) {
+			board[i] = base.board[i];
+		}
+
+		allSymmetries = base.allSymmetries;
+		
+		totalRounds = base.totalRounds+1;
+
+		board[following] = base.nextValueInCell();
+
+		if(base.statesOfAnyGame != StatesOfAnyGame.Playing) {
+			statesOfAnyGame = base.statesOfAnyGame;
+		} else {
+			GameStateSetter(following);
+		}
+
+		SymmetryReset();
+	}
