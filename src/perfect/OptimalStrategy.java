c2dbf8a26d81e7f3f7b1091905b540e38877835f
viveksharma180 <sharma.viv@northeastern.edu>
2022-04-15 21:53:18 -0400

    PSA_FinalProject: Initialized variables for OptimalStrategy.java
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- src/perfect/OptimalStrategy.java
+++ src/perfect/OptimalStrategy.java
@@ -5,5 +5,11 @@ import src.game.TicTacToeGame;
 import src.utils.Utils;
 
 public class Optimal_Tic_Tac_Toe extends TicTacToeGame {
+    public static final int noSetting = 0;
+    public static final int conditionWin  = 1;
+    public static final int conditionLose = 2;
+    public static final int conditionDraw = 3;
+    private int[] possibleOutcomes;
+    private int outcomeOfGame;
     
 }
