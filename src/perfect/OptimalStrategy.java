bfc846e21f0641e2d6c0e66e9b9a8652d463029a
viveksharma180 <sharma.viv@northeastern.edu>
2022-04-15 22:04:24 -0400

    PSA_FinalProject: Rectified mistake in OptimalStrategy.java
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- src/perfect/OptimalStrategy.java
+++ src/perfect/OptimalStrategy.java
@@ -1,27 +1,12 @@
 package src.perfect;
 
+import src.game.Participant;
 import src.game.StatesOfAnyGame;
 import src.game.TicTacToeGame;
-import src.utils.Utils;
+import src.game.ValuesOfCell;
 
-public class Optimal_Tic_Tac_Toe extends TicTacToeGame {
-    public static final int noSetting = 0;
-    public static final int conditionWin  = 1;
-    public static final int conditionLose = 2;
-    public static final int conditionDraw = 3;
-    private int[] possibleOutcomes;
-    private int outcomeOfGame;
+import java.util.LinkedList;
 
-    public Optimal_Tic_Tac_Toe(){
-        super(3,3,3);
-        possibleOutcomes = new int[9];
-        outcomeOfGame = noSetting;
-    }
-
-    public Optimal_Tic_Tac_Toe(Optimal_Tic_Tac_Toe base, int following){
-        super(base,following);
-        possibleOutcomes = new int[9];
-        outcomeOfGame = noSetting;
-    }
+public class OptimalStrategy extends Participant {
     
 }
