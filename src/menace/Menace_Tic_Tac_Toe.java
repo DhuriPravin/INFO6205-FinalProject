352e247502a867fc1160d8ed6b02f5a5c568fc46
prathams99 <sahasrabuddhe.p@northeastern.edu>
2022-04-16 15:23:23 -0400

    PSA_FinalProject: Add constructor for MenaceTicTacToe
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

---
+++ src/menace/Menace_Tic_Tac_Toe.java
@@ -0,0 +1,17 @@
+package src.menace;
+
+import src.game.StatesOfAnyGame;
+import src.game.TicTacToeGame;
+import src.utils.Utils;
+
+public class Menace_Tic_Tac_Toe extends TicTacToeGame {
+
+	private int[] bead;
+
+	int alpha =8;//start
+
+	private int totalBeads;
+
+	private int playedCells;
+
+}
