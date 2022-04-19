cd6bfba6f75d25ed032a2fdc4bb252972362b533
viveksharma180 <sharma.viv@northeastern.edu>
2022-04-19 11:10:37 -0400

    PSA_FinalProject: Write rotateElementsWhenRowEqualsColumns in Utils.java
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- src/utils/Utils.java
+++ src/utils/Utils.java
@@ -19,5 +19,36 @@ public class Utils {
 
     public Utils() throws IOException {
     }
-    
+
+    public static void rotateElementsWhenRowEqualsColumns(int rows, int columns, int[] boardTransformation){
+        if(boardTransformation == null) {
+            throw new NullPointerException("Incorrect input: Null");
+        }
+        if (rows < 1 || columns < 1) {
+            throw new IllegalArgumentException("Incorrect input of row or column.");
+        }
+        if (rows * columns != boardTransformation.length) {
+            throw new IllegalArgumentException("rows * columns must be equal to the length of the matrix");
+        }
+        if (rows != columns) {
+            throw new IllegalArgumentException("Rows and columns must be same in a TicTacToe board.");
+        }
+        for (int currRow = 0; currRow < rows; currRow++) {
+
+            for (int currColumn = currRow; currColumn < columns - currRow - 1; currColumn++) {
+
+                int CRight = currColumn * columns + (rows - currRow - 1);
+                int bottomRow = (rows - currRow - 1) * columns + (columns - currColumn - 1);
+                int CLeft = (columns - currColumn - 1) * columns + currRow;
+
+                int temp = boardTransformation[currRow * rows + currColumn];
+
+                boardTransformation[currRow * rows + currColumn] = boardTransformation[CLeft];
+                boardTransformation[CLeft] = boardTransformation[bottomRow];
+                boardTransformation[bottomRow] = boardTransformation[CRight];
+                boardTransformation[CRight] = temp;
+            }
+        }
+
+    }
 }
