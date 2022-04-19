9505569108a12c1b21781d3becfe61c18f0a5874
prathams99 <sahasrabuddhe.p@northeastern.edu>
2022-04-18 20:16:49 -0400

    PSA_FinalProject: Handle exception for illegal game state
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

--- src/human/HumanInput.java
+++ src/human/HumanInput.java
@@ -2,4 +2,11 @@ package src.human;
 
 public class HumanInput extends Participant {
 
+	public  void start(TicTacToeGame ticTacToeGame) {
+
+		if(ticTacToeGame.getTotalRounds() == ticTacToeGame.rows*ticTacToeGame.columns){
+			throw new IllegalArgumentException("Game doesn't exist!");
+		}
+	}
+
 }
