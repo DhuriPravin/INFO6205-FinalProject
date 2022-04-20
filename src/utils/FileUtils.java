ad0501f1c24acc0b613935ab2506512ccd9d4457
Pravin Dhuri <dhuri.p@northeastern.edu>
2022-04-20 14:17:35 -0400

    PSA_FinalProject: Added writeResultsToFile in FileUtils.java
    Signed-off-by: Pravin Dhuri <dhuri.p@northeastern.edu>

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -30,4 +30,57 @@ public class FileUtils {
             }
         }
     }
+
+    public static void writeResultsToFile(String accelRow, long time){
+        PrintWriter printWriter = null;
+        String data = getTimestampString(time) + ", " + accelRow;
+
+        String dayFormat = "yyyy-MM-dd";
+        String hourFormat = "HH-z";
+        String minuteForma = "mm";
+
+        Date dateNow = new Date();
+        String dayDirectory = new SimpleDateFormat(dayFormat, Locale.US).format(dateNow);
+        String hourDirectory = new SimpleDateFormat(hourFormat, Locale.US).format(dateNow);
+        String minuteDirectory = new SimpleDateFormat(minuteForma, Locale.US).format(dateNow);
+
+        String watchAccelDataDirectory = "logs" + "/" + dayDirectory + "/" + hourDirectory + "/" + minuteDirectory + "/" + "Data.csv";
+        File watchAccelDataFile = new File(watchAccelDataDirectory);
+
+        if(!watchAccelDataFile.getParentFile().exists()){
+            watchAccelDataFile.getParentFile().mkdirs();
+        }
+
+        if(watchAccelDataFile.exists()) {
+            try {
+                printWriter =
+                        new PrintWriter(new BufferedWriter(new FileWriter(watchAccelDataFile, true)));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try{
+            if (watchAccelDataFile.createNewFile()){
+                String headerRow = "timestamp, unixtime, auc, watchWorn, sleepmode, wakemode, waketime, sleeptime";
+                if (printWriter != null) {
+                    printWriter.println(headerRow);
+                    if (printWriter.checkError()) {
+                    }
+                }
+            }else {
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (printWriter != null) {
+
+            printWriter.println(data);
+            if (printWriter.checkError()) {
+            }
+        }
+    }
+
 }
+
