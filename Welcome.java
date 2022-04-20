262d0b84a8c8e964dddb1a7e6994b72ec1745762
prathams99 <sahasrabuddhe.p@northeastern.edu>
2022-04-20 19:49:16 -0400

    PSA_FinalProject: Added gif to welcome user
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

---
+++ Welcome.java
@@ -0,0 +1,12 @@
+package src.ui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Welcome extends JWindow {
+
+    public static void main(String[] args) {
+        welcome();
+    }
+
+}
