6e158e71884c5be441a3258fecf92776860a97f2
viveksharma180 <sharma.viv@northeastern.edu>
2022-04-20 18:24:29 -0400

    PSA_FinalProject: Completed LOSTGIF.java
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- src/ui/LostGif.java
+++ src/ui/LostGif.java
@@ -4,4 +4,27 @@ import javax.swing.*;
 
 public class LostGif extends JWindow {
 
+    public static void main(String[] args) {
+        lost();
+    }
+
+    public static void lost() {
+        TicTacToeUI.dispatchWindows();
+        JLabel label = new JLabel(new ImageIcon(LostGif.class.getResource("lost.gif")));
+
+        JFrame f = new JFrame("You Lost");
+        f.getContentPane().add(label);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.pack();
+        f.setLocationRelativeTo(null);
+        f.setVisible(true);
+        f.toFront();
+        f.setAlwaysOnTop(true);
+        try {
+            Thread.sleep(4000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        f.setVisible(false);
+    }
 }
