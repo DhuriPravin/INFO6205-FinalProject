e75a5d9e49b59fdfe25bbfdb3a46fcb5a62dae6c
Pravin Dhuri <dhuri.p@northeastern.edu>
2022-04-21 18:45:20 -0400

    PSA_FinalProject: Adding train method to Execute_Tic_Tac_Toe.java
    Signed-off-by: Pravin Dhuri <dhuri.p@northeastern.edu>

--- Execute_Tic_Tac_Toe.java
+++ Execute_Tic_Tac_Toe.java
@@ -21,5 +21,33 @@ import java.util.Set;
 
 
 public class Execute_Tic_Tac_Toe {
-    
+
+    public static final int NumberOfTraining = 10000;
+    private static final Logger logger = LogManager.getLogger(Execute_Tic_Tac_Toe.class);
+
+    private static void train(Participant[] participants) {
+
+        BasicConfigurator.configure();
+        flushAllLogs();
+
+        int NumberOfTrainings = NumberOfTraining;
+
+        for(int i=1;i<NumberOfTrainings;i++){
+            TicTacToeGame ticTacToeGame = new TicTacToeGame();
+            int chance = 0;
+            participants[chance%2].startNewGame(ValuesOfCell.X);
+            participants[(chance+1)%2].startNewGame(ValuesOfCell.O);
+            while(ticTacToeGame.getStateOfGame() == StatesOfAnyGame.Playing) {
+                participants[chance%2].start(ticTacToeGame);
+                chance++;
+            }
+            String data = i + ", " + chance + ", " + participants[0].showData() + ", " + ticTacToeGame.getStateOfGame();
+            FileUtils.writeResultsToFile(data, System.currentTimeMillis());
+            participants[0].gameOver(ticTacToeGame.getStateOfGame());
+            participants[1].gameOver(ticTacToeGame.getStateOfGame());
+        }
+        System.out.println("Menace: " + participants[0]) ;
+        System.out.println("Player: " + participants[1]) ;
+
+    }
 }
