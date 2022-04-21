7f64e2d983a611ed489d6cde3868f00fc3e8ce61
Pravin Dhuri <dhuri.p@northeastern.edu>
2022-04-21 15:30:18 -0400

    PSA_FinalProject: Adding TestCase to RunTests.java
    Signed-off-by: Pravin Dhuri <dhuri.p@northeastern.edu>

--- test/RunTests.java
+++ test/RunTests.java
@@ -29,5 +29,25 @@ public class RunTests {
     public void checkHorizontalFlips2X2() {
         Assert.assertEquals("[2, 3, 0, 1]", UtilsToString.checkFlipElementsHorizontal(2, 2));
     }
+    
+    @Test
+    public void checkVerticalFlip2X2(){
+        Assert.assertEquals("[1, 0, 3, 2]", UtilsToString.checkFlipElementsVertical(2, 2));
+    }
+
+    @Test
+    public void checkRotation2X2(){
+        Assert.assertEquals("[2, 0, 3, 1]", UtilsToString.checkElementsRotation(2, 2));
+    }
+
+    @Test
+    public void checkHorizontalFlips3X3() {
+        Assert.assertEquals("[6, 7, 8, 3, 4, 5, 0, 1, 2]", UtilsToString.checkFlipElementsHorizontal(3, 3));
+    }
+
+    @Test
+    public void checkVerticalFlip3X3(){
+        Assert.assertEquals("[2, 1, 0, 5, 4, 3, 8, 7, 6]", UtilsToString.checkFlipElementsVertical(3, 3));
+    }
 
 }
