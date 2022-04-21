df7b86f85a769e4ba356f46738ffa8a4430fbe3d
Pravin Dhuri <dhuri.p@northeastern.edu>
2022-04-20 21:40:21 -0400

    PSA_FinalProject: Updating TicTacToeUI.java
    Signed-off-by: Pravin Dhuri <dhuri.p@northeastern.edu>

--- src/ui/TicTacToeUI.java
+++ src/ui/TicTacToeUI.java
@@ -16,3 +16,14 @@ import java.awt.event.KeyEvent;
 import java.io.Console;
 import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
+
+public class TicTacToeUI extends JWindow {
+
+    public static void dispatchWindows() {
+        java.awt.Window win[] = java.awt.Window.getWindows();
+        for (int i = 0; i < win.length; i++) {
+            win[i].dispose();
+        }
+    }
+
+}
