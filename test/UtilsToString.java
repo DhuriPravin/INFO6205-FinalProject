f1f934efc5dfa5a2a60b963d357c8daa8a7eb265
viveksharma180 <sharma.viv@northeastern.edu>
2022-04-21 18:16:18 -0400

    PSA_FinalProject: Write checkFlipElementsHorizontal in UtilsToString.java
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- test/UtilsToString.java
+++ test/UtilsToString.java
@@ -1,5 +1,13 @@
 import Utils.Utils;
 
 public class UtilsToString {
-    
+    public static String checkFlipElementsHorizontal(int rows, int columns) {
+        int[] test;
+        test = new int[rows*columns];
+        for(int i = 0 ; i < test.length; i++){
+            test[i] = i;
+        }
+        Utils.flipElementsHorizontal(rows,columns,test);
+        return java.util.Arrays.toString(test);
+    }
 }
