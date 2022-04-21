7b6b4d2110b75b6ccfc1b7236c61d8b5224138a2
prathams99 <sahasrabuddhe.p@northeastern.edu>
2022-04-21 09:47:01 -0400

    PSA_FinalProject: Add junit test cases for checking menace algorithms
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

--- test/RunTests.java
+++ test/RunTests.java
@@ -3,4 +3,31 @@ import org.junit.Assert;
 
 public class RunTests {
 
+	@Test
+    public void checkHorizontalFlipsIncorrect() {
+        Throwable throwable = Assert.assertThrows(IllegalArgumentException.class, ()-> {
+            UtilsToString.checkFlipElementsHorizontal(0, 0);
+        });
+        Assert.assertTrue(throwable.getMessage().contains("Incorrect input of currRow or column."));
+    }
+
+    @Test
+    public void checkVerticalFlipIncorrect(){
+        Throwable throwable = Assert.assertThrows(IllegalArgumentException.class, ()-> {
+            UtilsToString.checkFlipElementsVertical(0, 0);
+        });
+        Assert.assertTrue(throwable.getMessage().contains("Incorrect input of currRow or column."));    }
+
+    @Test
+    public void checkRotationIncorrect(){
+        Throwable throwable = Assert.assertThrows(IllegalArgumentException.class, ()-> {
+            UtilsToString.checkElementsRotation(0, 0);
+        });
+        Assert.assertTrue(throwable.getMessage().contains("Incorrect input of row or column."));
+    }
+    @Test
+    public void checkHorizontalFlips2X2() {
+        Assert.assertEquals("[2, 3, 0, 1]", UtilsToString.checkFlipElementsHorizontal(2, 2));
+    }
+
 }
