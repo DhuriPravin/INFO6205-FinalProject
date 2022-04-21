2b50f3b3443b68bc41bb37095e641adcec7b03fc
viveksharma180 <sharma.viv@northeastern.edu>
2022-04-21 18:36:17 -0400

    PSA_FinalProject: Write checkFlipElementsVertical and checkElementsRotation in UtilsToString.java
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- test/UtilsToString.java
+++ test/UtilsToString.java
@@ -10,4 +10,24 @@ public class UtilsToString {
         Utils.flipElementsHorizontal(rows,columns,test);
         return java.util.Arrays.toString(test);
     }
+
+    public static String checkFlipElementsVertical(int rows, int columns){
+        int[] test;
+        test = new int[rows*columns];
+        for(int i = 0 ; i < test.length; i++){
+            test[i] = i;
+        }
+        Utils.flipElementsVertical(rows,columns,test);
+        return java.util.Arrays.toString(test);
+    }
+
+    public static String checkElementsRotation(int rows, int columns){
+        int[] test;
+        test = new int[rows*columns];
+        for(int i = 0 ; i < test.length; i++){
+            test[i] = i;
+        }
+        Utils.rotateElementsWhenRowEqualsColumns(rows,columns,test);
+        return java.util.Arrays.toString(test);
+    }
 }
