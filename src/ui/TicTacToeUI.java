bcfb3354be228ac40b9909ffeb18883aa6dc6d52
prathams99 <sahasrabuddhe.p@northeastern.edu>
2022-04-21 02:59:10 -0400

    PSA_FinalProject: Update TicTacToe UI and fix the previous issue
    - Add focus to window when game is being played
    
    Signed-off-by: prathams99 <sahasrabuddhe.p@northeastern.edu>

--- src/ui/TicTacToeUI.java
+++ src/ui/TicTacToeUI.java
@@ -21,7 +21,7 @@ public class TicTacToeUI extends JWindow {
 
     public static void dispatchWindows() {
         java.awt.Window win[] = java.awt.Window.getWindows();
-        for (int i = 0; i < win.length; i++) {
+        for(int i=0;i<win.length;i++){
             win[i].dispose();
         }
     }
@@ -52,26 +52,28 @@ public class TicTacToeUI extends JWindow {
     static JButton button = null;
 
     public static void showUI() {
-        final JFrame frame = new JFrame();
+        final JFrame frame = new JFrame ();
         frame.getContentPane().setBackground(Color.BLACK);
         frame.setTitle("Tic Tac Toe");
         frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
         frame.getContentPane().removeAll();
         frame.repaint();
 
-        final JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 5)) {
-            public Dimension getPreferredSize() {
-                Dimension ps = super.getPreferredSize();
+        final JPanel panel = new JPanel ( new FlowLayout( FlowLayout.LEFT, 5, 5 ) )
+        {
+            public Dimension getPreferredSize ()
+            {
+                Dimension ps = super.getPreferredSize ();
                 ps.width = 0;
                 return ps;
             }
         };
         panel.setBackground(Color.decode("#000000"));
-        frame.add(new JScrollPane(panel));
+        frame.add ( new JScrollPane ( panel ) );
 
-        frame.setSize(445, 240);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
+        frame.setSize ( 445, 240 );
+        frame.setDefaultCloseOperation ( JFrame.EXIT_ON_CLOSE );
+        frame.setLocationRelativeTo ( null );
 
         //frame2.setVisible(true);
 
@@ -81,7 +83,7 @@ public class TicTacToeUI extends JWindow {
 //                {
 //                    public void run ()
 //                    {
-        frame.setVisible(true);
+        frame.setVisible ( true );
 //                    }
 //                } );
 //            } catch (InvocationTargetException e) {
@@ -91,18 +93,18 @@ public class TicTacToeUI extends JWindow {
 //            e.printStackTrace();
 //        }
 
-        final JDialog load = new JDialog(frame);
+        final JDialog load = new JDialog ( frame );
 
-        JPanel panel2 = new JPanel(new BorderLayout());
-        panel2.setBorder(BorderFactory.createEmptyBorder(15, 15, 15, 15));
-        load.add(panel2);
+        JPanel panel2 = new JPanel ( new BorderLayout() );
+        panel2.setBorder ( BorderFactory.createEmptyBorder ( 15, 15, 15, 15 ) );
+        load.add ( panel2 );
 
-        final JProgressBar progressBar = new JProgressBar(0, 100);
-        panel2.add(progressBar);
+        final JProgressBar progressBar = new JProgressBar ( 0, 100 );
+        panel2.add ( progressBar );
 
-        load.setModal(false);
-        load.pack();
-        load.setLocationRelativeTo(frame);
+        load.setModal ( false );
+        load.pack ();
+        load.setLocationRelativeTo ( frame );
 
 //        try {
 //            SwingUtilities.invokeAndWait ( new Runnable ()
@@ -121,5 +123,51 @@ public class TicTacToeUI extends JWindow {
         // = new JButton (String.valueOf(src.utils.Utils.getText().get(i)));
         setButtonColor();
 
+        for ( int i = 0; i < 9; i++ )
+        {
+            button = buttons.get(i);
+            button.setText(Utils.inputs.get(i));
+            button.setPreferredSize(new Dimension(136, 60));
+            button.setBackground(Color.decode("#212121"));
+            button.setOpaque(true);
+            button.setForeground(Utils.getText().get(i).equals("X") ? Color.red : Color.green);
+            final int finalI = i;
+            setButtonColor();
+
+            // Updating panel and progress in EDT
+            JButton finalButton = button;
+            panel.add(finalButton);
+            finalButton.revalidate ();
+            finalButton.repaint();
+            progressBar.setValue ( finalI );
+
+        }
+
+        JLabel status = new JLabel();
+        status.setText("");
+
+        load.setVisible(false);
+        progressBar.setVisible(false);
+        setButtonColor();
+        frame.toFront();
+        frame.setAlwaysOnTop(true);
+    }
+
+    public static void setButtonColor() {
+        for (int i = 0; i < 9; i++) {
+            buttons.get(i).setBackground(Color.decode("#212121"));
+            buttons.get(i).setContentAreaFilled( false );
+        }
+    }
+
+    private static void writeToConsole(int cellValue) {
+
+    }
+
+    @Override
+    public final void setVisible(boolean v) {
+        // Make sure we grab the focus so that key events don't go astray.
+        requestFocus();
+        super.setVisible(v);
     }
 }
