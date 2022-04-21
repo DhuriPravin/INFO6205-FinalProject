efc38675d812ffdfd91dba4cba33b57780985974
viveksharma180 <sharma.viv@northeastern.edu>
2022-04-21 17:38:48 -0400

    PSA_FinalProject: Completed test/RunTests.java
    Signed-off-by: viveksharma180 <sharma.viv@northeastern.edu>

--- test/RunTests.java
+++ test/RunTests.java
@@ -50,4 +50,23 @@ public class RunTests {
         Assert.assertEquals("[2, 1, 0, 5, 4, 3, 8, 7, 6]", UtilsToString.checkFlipElementsVertical(3, 3));
     }
 
+    @Test
+    public void checkRotation3X3(){
+        Assert.assertEquals("[6, 3, 0, 7, 4, 1, 8, 5, 2]", UtilsToString.checkElementsRotation(3, 3));
+    }
+
+    @Test
+    public void checkHorizontalFlips4X4() {
+        Assert.assertEquals("[12, 13, 14, 15, 8, 9, 10, 11, 4, 5, 6, 7, 0, 1, 2, 3]", UtilsToString.checkFlipElementsHorizontal(4, 4));
+    }
+
+    @Test
+    public void checkVerticalFlip4X4(){
+        Assert.assertEquals("[3, 2, 1, 0, 7, 6, 5, 4, 11, 10, 9, 8, 15, 14, 13, 12]", UtilsToString.checkFlipElementsVertical(4, 4));
+    }
+    @Test
+    public void checkRotation4X4(){
+        Assert.assertEquals("[12, 8, 4, 0, 13, 9, 5, 1, 14, 10, 6, 2, 15, 11, 7, 3]", UtilsToString.checkElementsRotation(4, 4));
+    }
+
 }
